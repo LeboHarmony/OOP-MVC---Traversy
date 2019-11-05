13fab48ae5c2dba621f3dd8a0e719b82ab05102c
Lebohang KEBETHI <lkebethi@c4r7s11.wethinkcode.co.za>
2019-11-05 08:46:56 +0200

    Initial commit

---
+++ app/config/config.php
@@ -0,0 +1,15 @@
+<?php
+  // DB Params
+  define('DB_HOST', 'localhost');
+  define('DB_USER', 'root');
+  define('DB_PASS', '83773623');
+  define('DB_NAME', 'camapp');
+
+  // App Root
+  define('APPROOT', dirname(dirname(__FILE__)));
+  // URL Root
+  define('URLROOT', 'http://localhost:8080/CamApp');
+  // Site Name
+  define('SITENAME', 'CamApp');
+  // App Version
+  define('APPVERSION', '1.0.0');
